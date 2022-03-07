011174e7605d0f473ef10bbc53ff2bf4d55e552d
Samik Roy <samik.n.roy@gmail.com>
2022-03-07 21:23:58 +0530

    Update sample.ps1

--- sample.ps1
+++ sample.ps1
@@ -1,5 +1,5 @@
 $Response = Invoke-WebRequest -URI https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519
 $DownloadLink = $Response.Links |  where {$_.href.Contains("/ServiceTags_Public")} |select href
-$URL = $DownloadLink.href
+$URL = [System.Uri]$DownloadLink.href
 $jsonFileData = Invoke-WebRequest -Method Get -URI $URL | ConvertFrom-Json
 $jsonFileData | ConvertTo-Json -depth 100 | Out-File ".\servicetags.json"
