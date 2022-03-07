36112d573acb66278c6c5f4c7814375e5ab4a1e3
Samik Roy <samik.n.roy@gmail.com>
2022-03-07 20:38:53 +0530

    Create sample.ps1

---
+++ sample.ps1
@@ -0,0 +1,9 @@
+$Response = Invoke-WebRequest -URI https://www.bing.com/search?q=how+many+feet+in+a+mile
+$Response.InputFields | Where-Object {
+    $_.name -like "* Value*"
+} | Select-Object Name, Value
+
+name       value
+----       -----
+From Value 1
+To Value   5280
